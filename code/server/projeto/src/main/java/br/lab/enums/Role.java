f9c9a705717155d8da64c8342e6c53feb2ed1af0
gabi <gabialvarenga4@gmail.com>
2025-08-25 14:40:20 -0300

    feat: add enums

---
+++ code/server/projeto/src/main/java/br/lab/enums/Role.java
@@ -0,0 +1,7 @@
+package br.lab.enums;
+
+public enum Role {
+    ALUNO,
+    PROFESSOR,
+    SECRETARIA
+}
---
+++ code/server/projeto/src/main/java/br/lab/enums/StatusMatricula.java
@@ -0,0 +1,6 @@
+package br.lab.enums;
+
+public enum StatusMatricula {
+    ATIVA,
+    CANCELADA
+}
